f8697c79d23da8b354edaeea2b392e2736994d5e
Matheus Valiente Souza <matheusvalientesouza@hotmail.com>
2018-02-27 08:25:03 -0300

    Builing service using the solution, to fix missing reference.

--- src/SCIQuoting.Webapi/Program.cs
+++ src/SCIQuoting.Webapi/Program.cs
@@ -19,10 +19,6 @@ namespace SCIQuoting.Webapi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((builderContext, config) =>
-                {
-                    config.AddJsonFile("appsettings.json");
-                })
                 .UseStartup<Startup>()
                 .ConfigureLogging((hostingContext, builder) =>
                 {
