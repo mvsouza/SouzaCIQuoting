2dfcbaa7f05abc1cdf2fa8d4a1acd2ff4c11a482
Matheus Valiente Souza <matheusvalientesouza@hotmail.com>
2018-02-28 23:11:21 -0300

    Fix on api_key variable.

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -1,4 +1,4 @@
 docker-compose build --force-rm
-docker login --username=_ --password=$(api_key) registry.heroku.com
+docker login --username=_ --password=$api_key registry.heroku.com
 docker tag souzaciquoting.webapi registry.heroku.com/souzaciquoting/web
 docker push registry.heroku.com/souzaciquoting/web
