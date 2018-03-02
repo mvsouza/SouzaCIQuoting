925f7d3f7833baae1c078f4bf0e049ceadeae7a0
Matheus Valiente Souza <matheusvalientesouza@hotmail.com>
2018-03-01 22:02:24 -0300

    Fixed tests for the use of memory cache.

--- test/UnitTest/SCIQuoting.Webapi.Test/QuotingRequestControllerTests.cs
+++ test/UnitTest/SCIQuoting.Webapi.Test/QuotingRequestControllerTests.cs
@@ -7,27 +7,55 @@ using SCIQuoting.Webapi.Infrastructure.Repositories;
 using SCIQuoting.Webapi.Application.Models;
 using System;
 using Xunit;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
 
 namespace SCIQuoting.Webapi.Test
 {
+    public class CacheEntryMock : ICacheEntry
+    {
+        public object Key => new object();
+
+        public object Value { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+        public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
+        public TimeSpan? SlidingExpiration { get; set; }
+        public IList<IChangeToken> ExpirationTokens => throw new NotImplementedException();
+
+        public IList<PostEvictionCallbackRegistration> PostEvictionCallbacks => throw new NotImplementedException();
+
+        public CacheItemPriority Priority { get; set; }
+        public long? Size { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
     public class QuotingRequestControllerTest
     {
         private Mock<HttpContext> _contextHttpMock;
         private Mock<IEventBus> _eventBus;
         private Mock<IInsuranceQuotingRequestRepository> _repository;
-        public QuotingRequestControllerTest(){
+        private Mock<IMemoryCache> _memoryCache;
+        public QuotingRequestControllerTest()
+        {
             _eventBus = new Mock<IEventBus>();
             _repository = new Mock<IInsuranceQuotingRequestRepository>();
             _contextHttpMock = new Mock<HttpContext>();
+            _memoryCache = new Mock<IMemoryCache>();
         }
-
         [Fact]
         public async void Return_New_Id()
         {
-            var ctrl = new QuotingRequestController(_eventBus.Object, _repository.Object);
+            var ctrl = new QuotingRequestController(_eventBus.Object, _repository.Object, _memoryCache.Object);
             ctrl.ControllerContext.HttpContext = _contextHttpMock.Object;
+            _memoryCache.Setup(m => m.CreateEntry(It.IsAny<Guid>()))
+                        .Returns(new CacheEntryMock());
             var actionResult = await ctrl.Post(new QuoteRequestViewModel());
             var res = Assert.IsType<OkObjectResult>(actionResult);
+            //_memoryCache.Verify(m => m.Set<InsuranceQuotingRequest>(It.IsAny<Guid>(), It.IsAny<InsuranceQuotingRequest>()));
             Assert.IsAssignableFrom<Guid>(res.Value);
         }
     }
